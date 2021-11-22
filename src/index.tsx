80b12a398fc11860fe533f6cb2d174d6aba24cfb
a <a>
2021-11-22 15:24:00 -0500

    working basic routes with react

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles.css';
+// import App from './App';
+import {Home, Dashboard, SignIn} from './components'
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home title="Drones Inventory"/>} />
+
+        <Route path='/dashboard' element={<Dashboard />} />
+
+        <Route path='SignIn' element={<SignIn />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
